0a228aff38257b9d2d18e205c61053da0e5ea112
tim1207 <timlin891207@gmail.com>
2021-11-23 15:16:16 +0800

    add api list

--- app.js
+++ app.js
@@ -16,5 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/order', productsRouter);
 
 module.exports = app;
---
+++ routes/products.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+//product
+// find all
+router.get("/",(req,res,next)=>{
+
+});
+//find 單一種類
+router.get("/search",(req,res,next)=>{
+
+});
+//insert
+router.post("/",(req,res,next)=>{
+
+});
+router.delete("/:id",(req,res,next)=>{
+
+});
+//modify
+router.put("/:id",(req,res,next)=>{
+
+});
+module.exports = router;
