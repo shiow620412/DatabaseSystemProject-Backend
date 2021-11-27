12f07b214b1f2558e16ebe285ec7f8beb26f0fd7
tim1207 <timlin891207@gmail.com>
2021-11-27 11:54:36 +0800

    add api of user select

---
+++ src/server/controller/user.controller.js
@@ -0,0 +1,31 @@
+import userModule from "../module/user.module.js";
+
+/* User  POST 登入(Login) */
+const userLogin = (req, res, next) => {
+    // 取得帳密
+    userModule.Login(req.body).then((result) => {
+      res.send(result); // 成功回傳result結果
+    }).catch((error) => { next(error) }); // 失敗回傳錯誤訊息
+  };
+  
+
+const getUser = (req, res, next) => {
+  // console.log(typeof req.query["page"])
+  userModule.selectUser(req.user,req.query["page"]).then((result) => {
+    res.send(result); // 成功回傳result結果
+  }).catch((error) => { next(error) }); // 失敗回傳錯誤訊息
+};
+
+/* User  findPassword */
+const findPassword = (req, res, next) => {
+  userModule.findBackPassword(req.body).then((result) => {
+    res.send(result); // 成功回傳result結果
+  }).catch((error) => { next(error) }); // 失敗回傳錯誤訊息
+};
+
+export default 
+{
+    userLogin,
+    getUser,
+    findPassword
+}
--- src/server/route/user.route.js
+++ src/server/route/user.route.js
@@ -4,18 +4,21 @@ var router = express.Router();
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send(req.method+" "+req.originalUrl);
-});
+// router.get('/', function(req, res, next) {
+//     res.send(req.method+" "+req.originalUrl);
+// });
+
+router.post('/', middlewareService.verifyToken, userController.getUser ); 
+
 router.post('/register', function(req, res, next) {
     res.send(req.method+" "+req.originalUrl);
 });
-router.post('/login', function(req, res, next) {
-    res.send(req.method+" "+req.originalUrl);
-}); 
-router.get('/findPassword', function(req, res, next) {
-    res.send(req.method+" "+req.originalUrl);
-});
+router.post('/login', middlewareService.verifyToken, userController.userLogin ); 
+
+// router.get('/findPassword', function(req, res, next) {
+//     res.send(req.method+" "+req.originalUrl);
+// });
+router.post('/findPassword', userController.findPassword ); 
 
 router.post('/buy', function(req, res, next) {
     res.send(req.method+" "+req.originalUrl);
