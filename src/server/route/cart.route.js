54951ecebe6616a1cf9d0f1000ad540401ed4805
tim1207 <timlin891207@gmail.com>
2021-12-10 09:29:49 +0800

    modify cart route

--- src/server/route/cart.route.js
+++ src/server/route/cart.route.js
@@ -10,5 +10,6 @@ router.delete("/", middleware.verifyToken, cartController.removeProduct);
 //TODO: 要判斷庫存
 //post the product into shoppingCart
 router.post("/", middleware.verifyToken, cartController.putProduct); 
-
+// modify product 數量 quantity
+router.put("/", middleware.verifyToken, cartController.addQuantity); 
 export default router;
