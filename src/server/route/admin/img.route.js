e08c22687274c7679f5f4ebeee99dc64f69dbbee
Wind-PC <shiow620412@gmail.com>
2021-12-16 01:10:56 +0800

    add api of upload/load image

--- src/server/route/admin/img.route.js
+++ src/server/route/admin/img.route.js
@@ -1,10 +1,14 @@
 import express from "express";
+import multer  from 'multer';
+import imgController from "../../controller/admin/img.controller.js";
+const storage = multer.memoryStorage()
+const upload = multer({storage:storage })
+
+
 const router = express.Router();
 
 
-router.post("/upload", (req, res, next) => {
-    res.send(req.method + " " + req.originalUrl);
-});
+router.post("/upload", upload.single("image"), imgController.uploadImg);
 
 
 //
