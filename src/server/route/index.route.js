a5c4b47851ff75adf49773a566686e0c0cf4b144
Wind-PC <shiow620412@gmail.com>
2022-01-10 22:43:40 +0800

    add api of getCategory

--- src/server/route/index.route.js
+++ src/server/route/index.route.js
@@ -4,10 +4,11 @@ import product from "./product.route.js"
 import user from "./user.route.js"
 import cart from "./cart.route.js"
 import admin from "./admin/index.route.js"
+import category from "./category.route.js"
 import middleware from "../helper/middleware.js";
 const router = express.Router();
 
-
+router.use("/category", category)
 router.use("/products",product);
 router.use("/user",user);
 router.use(middleware.verifyToken);
